fb80b81eb16cbac4fa2a6301e9ec512e8c42fc59
Ryan McNair <ryan.mcnair@gmail.com>
2021-02-08 19:50:34 -0600

    initial setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DufflinMunder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
